76d216cbfeb7f1fc26c2a1d025a635323dee7f3f
Vit Wits <vitwits@gmail.com>
2024-06-04 17:38:58 +0200

    base backend project

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response }  from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("MongoDB Connected!"));
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/test", async (req: Request, res: Response) => {
+    res.json({ message: "Hello World!" });
+})
+
+app.listen(7000, () => console.log("Listening on localhost:7000"));
