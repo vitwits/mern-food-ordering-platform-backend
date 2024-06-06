d0501a268cde2385fa2f75d80588c4a86791d7a2
Vit Wits <vitwits@gmail.com>
2024-06-06 17:48:01 +0200

    added user model, create user route and controller

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
-import express, { Request, Response }  from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import MyUserRoute from "./routes/MyUserRoute";
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("MongoDB Connected!"));
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("MongoDB Connected!"));
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/test", async (req: Request, res: Response) => {
-    res.json({ message: "Hello World!" });
-})
+app.use("/api/my/user", MyUserRoute);
 
 app.listen(7000, () => console.log("Listening on localhost:7000"));
