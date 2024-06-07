82a1366da9d0d8dfd91e7682e32366147ae02982
Vit Wits <vitwits@gmail.com>
2024-06-07 10:51:51 +0200

    added update user controller, added auth for backend calls, added express validation for user fields, added route for updating user

--- src/controllers/MyUserController.ts
+++ src/controllers/MyUserController.ts
@@ -16,13 +16,37 @@ const createCurrentUser = async (req: Request, res: Response) => {
     const newUser = new User(req.body);
     await newUser.save();
 
-    res.status(201).json(newUser.toObject())
+    res.status(201).json(newUser.toObject());
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Error creating user" });
   }
 };
 
+const updateCurrentUser = async (req: Request, res: Response) => {
+  try {
+    const { name, addressLine1, country, city } = req.body;
+    const user = await User.findById(req.userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User doesn't exists" });
+    }
+
+    user.name = name;
+    user.addressLine1 = addressLine1;
+    user.city = city;
+    user.country = country;
+
+    await user.save();
+
+    res.send(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error updating user" });
+  }
+};
+
 export default {
   createCurrentUser,
+  updateCurrentUser,
 };
--- src/routes/MyUserRoute.ts
+++ src/routes/MyUserRoute.ts
@@ -1,8 +1,17 @@
+import { validateMyUserRequest } from "./../middleware/validation";
 import express from "express";
 import MyUserController from "../controllers/MyUserController";
+import { jwtCheck, jwtParse } from "../middleware/auth";
 
 const router = express.Router();
 
-router.post("/", MyUserController.createCurrentUser)
+router.post("/", jwtCheck, MyUserController.createCurrentUser);
+router.put(
+  "/",
+  jwtCheck,
+  jwtParse,
+  validateMyUserRequest,
+  MyUserController.updateCurrentUser
+);
 
 export default router;
